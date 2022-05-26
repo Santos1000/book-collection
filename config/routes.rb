40be24d7e23539b1f922c1e8b221fa957a99485b
barbara santos <barbaransantos1@gmail.com>
2022-05-26 16:15:38 -0300

    add name to user and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :books, only: [ :index ]  #GET /api/v1/books
+      resources :books, only: [:index]
     end
   end
   root to: 'pages#home'
