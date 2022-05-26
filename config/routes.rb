9975e01bb9bcb9b980a71056c2a1ed20d6c088c0
barbara santos <barbaransantos1@gmail.com>
2022-05-26 11:08:03 -0300

    setup erro message json and router v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api, defaults: { format: :json } do #GET /api/v1/books
+      resources :books, only: [ :index ]
+    end
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
