e58225e3262f226878b567777ab1d7a20c13bb79
barbara santos <barbaransantos1@gmail.com>
2022-05-27 08:25:41 -0300

    add token to users

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise'
 
 gem 'pundit'
 
+gem 'simple_token_authentication'
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form', github: 'heartcombo/simple_form'
--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,4 +7,15 @@ class Api::V1::BooksController < Api::V1::BaseController
     @book = Book.find(params[:id])
     authorize @book
   end
+
+  # def create
+  #   @book = Book.new(book_params)
+  #   @book.user = current_user
+  #   authorize @book
+  #   if @book.save
+  #     render :show, status: :created
+  #   else
+  #     render_error
+  #   end
+  # end
 end
